c32c1bffa7f5abb3d6d8a9fb79ee04252c312ba3
Jimmi Elofsson <jimmielofsson@gmail.com>
2013-09-11 14:04:22 +0200

    Bugfix.

--- set_feedback_to_image.php
+++ set_feedback_to_image.php
@@ -1,5 +1,5 @@
 <?php
-	$image_folder = 'images/';
+
 	// HEADER:s
 	header('Access-Control-Allow-Origin: *'); 
 	header('Access-Control-Allow-Methods: GET, POST'); 
@@ -13,32 +13,16 @@
 	}
 	else 
 	{
+		$dateTime = date("Y-m-d H:i:s");
 		$text = (isset($_POST['text'])) ? $_POST['text'] : $_GET['text'];
 		$image = (isset($_POST['image'])) ? $_POST['image'] : $_GET['image'];
-		/*
-		if(substr($image, 0, strlen($image_folder)) != $image_folder) // Check that we have image folder in string..
-		{
-			$image = $image_folder . $image; 
-		}
-		*/
+		$image = array_pop(explode('/', $image));
 	}
 	
 	// OK .. GO !	
-	$list = Array();
-	$sql = "SELECT * FROM pictures WHERE location='$image' LIMIT 0,1"; // SQL 
+	$list = Array();	
+	$sql = "UPDATE pictures SET feedback = '{$text}' AND datetime_feedback = '{$dateTime}' WHERE location like '%{$image}'"; // SQL 
 	$rs = ejecutarConsulta($sql);
-	if(mysql_num_rows($rs) > 0)
-	{
-		$row = mysql_fetch_assoc($rs);
-		$image = $row['location'];
-		$datetime = datetime('Y-m-d H:i:s');
-		$sql = "UPDATE pictures SET feedback = '$text' and datetime_feedback = '$datetime' WHERE location='$image'"; // SQL 
-		$rs = ejecutarConsulta($sql);
-		echo json_encode(array('success' => 'true'));
-	}
-	else
-	{
-		echo json_encode(array('success' => 'false'));
-	}
+	echo json_encode(array('success' => 'true'));
 
 ?>
